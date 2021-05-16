2abffb858682634ea87e2d0d4ec2b14b86103c1e
Sherpax <manuelsantiagovazquez.19@campuscamara.es>
2021-05-17 00:43:30 +0200

    gh
    gh

--- app/src/main/java/com/example/proyectorol/OpcionesUsuario.java
+++ app/src/main/java/com/example/proyectorol/OpcionesUsuario.java
@@ -15,24 +15,38 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.proyectorol.adapters.Adaptador;
+import com.example.proyectorol.pojos.Usuario;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.UserWriteRecord;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.GregorianCalendar;
+
 public class OpcionesUsuario extends AppCompatActivity {
-    ImageView img_foto;
-    TextView nombreUsuario;
+     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+     FirebaseDatabase baseDatos = FirebaseDatabase.getInstance();
+     DatabaseReference ref_usuario = baseDatos.getReference("Usuarios"); //Esto nos permite controlar las referencias al usuario por ID
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opciones_usuario);
       //  img_foto = findViewById(R.id.imagenPerfil);
       //  nombreUsuario = findViewById(R.id.nomUser);
+
+        ref_usuario.setValue(user.getUid());
 
         ViewPager2 viewPager2 = findViewById(R.id.viewPager);
         viewPager2.setAdapter(new Adaptador(this));
@@ -85,12 +99,43 @@ public class OpcionesUsuario extends AppCompatActivity {
             }
         });
 
-     //   final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+        usuarioUnico();
 
      //   nombreUsuario.setText(user.getDisplayName());
 
 
+    }
+    //Aquí hago referencias a la BD una vez hecho el login
+    private void usuarioUnico() {
+        //Lo hacemos una sola vez para evitar bucle de registros (en caso de usar push)
+        ref_usuario.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                //Creamos la referencia si no existe
+                if(!snapshot.exists()){
+                    Usuario _user = new Usuario();
+                    _user.setId(user.getUid());
+                    _user.setNombre(user.getDisplayName());
+                    _user.setNick("Probando");
+                    _user.setEmail(user.getEmail());
+                    _user.setFoto(user.getPhotoUrl().toString());
+                    _user.setEstado("Conectado"); //Modificar
+                    _user.setFecha("16/05/2021"); //Modificar
+                    _user.setHora("21:12");
+                    _user.setNuevosMensajes(1);
+                    _user.setSolicitudMensajes(2);
+                    //Guardamos los cambios
+                    ref_usuario.setValue(_user);
+                    Toast.makeText(OpcionesUsuario.this, "Yeah", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/proyectorol/pojos/Usuario.java
@@ -0,0 +1,110 @@
+package com.example.proyectorol.pojos;
+
+public class Usuario {
+    private String id;
+    private String nombre;
+    private String nick; //El que verán otros usuarios
+    private String email;
+    private String foto;
+    private String fecha;
+    private String hora;
+    private String estado; //Conectado, Desconectado
+    private int solicitudAmistad;
+    private int nuevosMensajes;
+
+    public Usuario() {
+    }
+
+    public Usuario(String id, String nombre, String nick, String email, String foto, String fecha, String hora, String estado, int solicitudAmistad, int nuevosMensajes) {
+        this.id = id;
+        this.nombre = nombre;
+        this.nick = nick;
+        this.email = email;
+        this.foto = foto;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.estado = estado;
+        this.solicitudAmistad = solicitudAmistad;
+        this.nuevosMensajes = nuevosMensajes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public int getSolicitudMensajes() {
+        return solicitudAmistad;
+    }
+
+    public void setSolicitudMensajes(int solicitudMensajes) {
+        this.solicitudAmistad = solicitudMensajes;
+    }
+
+    public int getNuevosMensajes() {
+        return nuevosMensajes;
+    }
+
+    public void setNuevosMensajes(int nuevosMensajes) {
+        this.nuevosMensajes = nuevosMensajes;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
