7854136a37373c8cb9614233b72c85a8957a8a7c
Sherpax <manuelsantiagovazquez.19@campuscamara.es>
2021-05-16 20:50:59 +0200

    df
    df

--- app/src/main/java/com/example/proyectorol/fragments/UsuarioFragment.java
+++ app/src/main/java/com/example/proyectorol/fragments/UsuarioFragment.java
@@ -1,5 +1,6 @@
 package com.example.proyectorol.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.proyectorol.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +30,19 @@ public class UsuarioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuario, container, false);
+        View view = inflater.inflate(R.layout.fragment_usuario, container, false);
+        //Nombre de usuario y perfil
+        TextView nombreUser = view.findViewById(R.id.nombreUser);
+        ImageView imgPerfil = view.findViewById(R.id.imagenPerfil);
+
+        final FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+        //Podemos hacer un shared preferences y una opción para cambiar el nombre etc
+        assert usuario != null;
+        nombreUser.setText(usuario.getDisplayName());
+        //TODO: ÁNGEL, BUSCA IMÁGENES PARA LOS PERSONAJES QUE QUIERAS USAR...
+        // (POR DEFECTO HE PUESTO UN VAMPIRO GRASIOSETE)
+        imgPerfil.setImageResource(R.drawable.vampirito);
+
+        return view;
     }
 }
