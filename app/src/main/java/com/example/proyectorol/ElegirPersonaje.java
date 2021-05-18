04c763f2463a62306ac1a7bc681eca9ca55a5246
AngelRRev <82054232+AngelRRev@users.noreply.github.com>
2021-05-18 07:54:29 +0200

    ¿¿¿???Por que no se ha subido esto con el otro commit xd

--- app/src/main/java/com/example/proyectorol/ElegirPersonaje.java
+++ app/src/main/java/com/example/proyectorol/ElegirPersonaje.java
@@ -15,7 +15,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ElegirPersonaje extends AppCompatActivity {
 
-    com.example.proyectorol.ficha.ListaClases ficha = new ListaClases();
+    com.example.proyectorol.ficha.ListaClases ficha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,34 +24,36 @@ public class ElegirPersonaje extends AppCompatActivity {
 
         CircleImageView imagenRaza = findViewById(R.id.imagenPerfil);
         Spinner spinerRaza = findViewById(R.id.razaSpin);
-
+        ficha= (ListaClases) getIntent().getSerializableExtra("Ficha");
         spinerRaza.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String[] clanes = getResources().getStringArray(R.array.clan);
+                ficha.setClan(clanes[position]);
                 switch (position){
                     case 0:
-                        imagenRaza.setImageResource(R.drawable.vampirito);
+                        imagenRaza.setImageResource(R.drawable.brujah);
                         break;
                     case 1:
-                        //imagenRaza.setImageDrawable(R.drawable);
+                        imagenRaza.setImageResource(R.drawable.malkavian);
                         break;
                     case 2:
-                     //   imagenRaza.setImageDrawable(R.drawable);
+                        imagenRaza.setImageResource(R.drawable.nosferatu);
                         break;
                     case 3:
-                     //   imagenRaza.setImageDrawable(R.drawable);
+                        imagenRaza.setImageResource(R.drawable.tremere);
                         break;
                     case 4:
-                     //   imagenRaza.setImageDrawable(R.drawable);
+                        imagenRaza.setImageResource(R.drawable.lasombra);
                         break;
                     case 5:
-                     //   imagenRaza.setImageDrawable(R.drawable);
+                        imagenRaza.setImageResource(R.drawable.tzimisce);
                         break;
                     case 6:
-                     //   imagenRaza.setImageDrawable(R.drawable);
+                        imagenRaza.setImageResource(R.drawable.assamita);
                         break;
                     case 7:
-                     //   imagenRaza.setImageDrawable(R.drawable);
+                        imagenRaza.setImageResource(R.drawable.gangrel);
                         break;
 
                 }
@@ -63,6 +65,19 @@ public class ElegirPersonaje extends AppCompatActivity {
 
         });
 
+        Spinner naturaleza = findViewById(R.id.spinner2);
+        naturaleza.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[] naturaleza = getResources().getStringArray(R.array.naturaleza);
+                ficha.setClan(naturaleza[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     public void irAtributos(View view) {
--- app/src/main/java/com/example/proyectorol/fragments/FichaInfoFragment.java
+++ app/src/main/java/com/example/proyectorol/fragments/FichaInfoFragment.java
@@ -5,35 +5,62 @@ import com.example.proyectorol.ElegirPersonaje;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.proyectorol.ElegirPersonaje;
 import com.example.proyectorol.R;
+import com.example.proyectorol.ficha.ListaClases;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class FichaInfoFragment extends Fragment {
 
+    private Map<String,ListaClases> fichas = new HashMap<>();
+    private ArrayList<ListaClases> fichasJugador=new ArrayList<>();
 
     public FichaInfoFragment() {
         // Required empty public constructor
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_ficha_info, container, false);
-        //
-        FloatingActionButton aniadirFicha = view.findViewById(R.id.creaFicha);
+        //Cargamos las fichas de la BD y las ponemos en el listView
 
+
+        FloatingActionButton aniadirFicha = view.findViewById(R.id.creaFicha);
         aniadirFicha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(view.getContext(),ElegirPersonaje.class);
+                com.example.proyectorol.ficha.ListaClases ficha = new ListaClases();
+                final FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+                ficha.setEmailJugador(usuario.getEmail());
+                intent.putExtra("Ficha",ficha);
                 startActivity(intent);
             }
         });
@@ -41,5 +68,37 @@ public class FichaInfoFragment extends Fragment {
         //
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        final FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseDatabase baseDatos = FirebaseDatabase.getInstance();
+        DatabaseReference ref_fichas = baseDatos.getReference("fichas");
+        //Con esto se coge toda la info de la tabla
+        ref_fichas.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                fichas = (Map<String, ListaClases>) snapshot.getValue(ListaClases.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+        ArrayList<String> nombreFichas= new ArrayList<>();
+        //La filtramos para dejar la relativa al usuario
+        for(Map.Entry<String,ListaClases> entry : fichas.entrySet()){
+            if(entry.getValue().getEmailJugador().equals(usuario.getEmail())){
+                fichasJugador.add(entry.getValue());
+                nombreFichas.add(entry.getKey());
+            }
+        }
+        ListView listafichas = getView().findViewById(R.id.listFichas);
+        ArrayAdapter adaptador = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1,
+            nombreFichas);
+        listafichas.setAdapter(adaptador);
     }
 }
